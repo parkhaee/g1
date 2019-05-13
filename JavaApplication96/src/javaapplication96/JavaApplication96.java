6488fa6f3602f6d1e940263fa0baaa6fba15ebac
parkhaee <parkhaee@sheridancollege.ca>
2019-05-13 14:21:50 -0400

    changed println into comment

---
+++ JavaApplication96/src/javaapplication96/JavaApplication96.java
@@ -0,0 +1,16 @@
+package javaapplication96;
+
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class JavaApplication96 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+}
